377f63ac0d962f15a52cdd8c3c8021b60e2fb8b5
Ashis Kumar <srimanashis@gmail.com>
2021-10-30 14:22:21 +0530

    utcToLocal added

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import date from 'date-and-time';
 const request = async (url, data = {}, authorization = 0) => {
 	let method = Object.keys(data).length > 0 ? "POST" : "GET";
 	let result;
@@ -323,13 +322,13 @@ const mysqlTableData = async (reqData, procName, procAction, { host, user, passw
 }
 
 const mysqlDate = (utc = true) => {
-	// const date = require("date-and-time");
+	const date = require("date-and-time");
 	const now = new Date();
 	return date.format(now, "YYYY-MM-DD HH:mm:ss", utc);
 };
 
 const utcToLocal = (dateTime, dtFormat = 'DD MMM YYYY hh:mmA') => {
-	// const date = require("date-and-time");
+	const date = require("date-and-time");
 	if (dateTime != '' && dateTime != null && dateTime != undefined) {
 		let dt = date.format(new Date(dateTime), 'YYYY/MM/DD HH:mm:ss')
 		let hr = date.format(new Date(dateTime), 'HH')
